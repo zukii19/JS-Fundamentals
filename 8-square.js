401524e730dda1c0953cedfefd6643e8e4e82a04
Zukiswa Zulu <zukizulu19@gmail.com>
2025-07-15 00:28:28 -0700

    Add 8-square.js - prints square of X of given size

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const size = parseInt(process.argv[2]);
+
+if (!size || size <= 0) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
+}
