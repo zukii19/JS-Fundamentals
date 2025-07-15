81c0f44052121a1ad9cf3d875997570a5b057b7e
Zukiswa Zulu <zukizulu19@gmail.com>
2025-07-15 00:33:16 -0700

    Add 9-add.js - adds two integers

---
+++ 9-add.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(add(a, b));
+}
